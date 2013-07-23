1208532473ce5536c488ec7d3d9462a62daefadf
Yilong Geng <gengyl08@stanford.edu>
2013-07-23 00:59:45 -0700

    osnt sw rate limiter

--- osnt/packet_generator_nic.py
+++ osnt/packet_generator_nic.py
@@ -37,23 +37,41 @@ class Port_Arbiter:
         self.iface = iface
         self.engine_list = engine_list
 
-    def merge_queues():
-        pos = [0]*len(engine_list);
-        pcap_list = [None]*len(engine_list);
-        for i in range(len(engine_list)):
-            pcap_list[i] = rdpcap(engine_list[i] + '.cap')
-        queues_num = range(len(engine_list))
+    def merge_queues(self):
+        pos = [0]*len(self.engine_list);
+        pcap_list = [None]*len(self.engine_list);
+        for i in range(len(self.engine_list)):
+            pcap_list[i] = rdpcap(self.engine_list[i] + '.cap')
+        queues_num = len(self.engine_list)
         pkts_num = sum(len(pkts) for pkts in pcap_list)
         pkts = [None]*pkts_num
         while(sum(pos)<pkts_num):
-            queue_id = 0
             for i in range(queues_num):
-                if(pos[i] < len(pcap_list[i]) and pcap_list[i][pos[i]].time < pcap_list[queue_id][pos[queue_id]]):
+                if(pos[i] < len(pcap_list[i])):
+                    queue_id = i
+                    break
+            for i in range(queues_num):
+                if(pos[i] < len(pcap_list[i]) and pcap_list[i][pos[i]].time < pcap_list[queue_id][pos[queue_id]].time):
                     queue_id = i
             pkts[sum(pos)] = pcap_list[queue_id][pos[queue_id]]
             pos[queue_id] = pos[queue_id] + 1
-        wrpcap(iface + '.cap', pkts)
+        wrpcap(self.iface + '.cap', pkts)
 
+
+class Rate_Limiter:
+    # rate in bps
+    def __init__(self, iface, rate):
+        self.iface = iface
+        self.rate = float(rate)
+        self.limit_rate()
+
+    def limit_rate(self):
+        pkts = rdpcap(self.iface + '.cap')
+        last_pkt_end_time = 0
+        for pkt in pkts:
+            pkt.time = max(pkt.time, last_pkt_end_time)
+            last_pkt_end_time = pkt.time + len(pkt)*8/self.rate
+        wrpcap(self.iface + '.cap', pkts)
 
 if __name__=="__main__":
 
@@ -65,3 +83,4 @@ if __name__=="__main__":
 
     arbiter = Port_Arbiter('eth1', ['cbr', 'poisson'])
     arbiter.merge_queues()
+    rate_limiter = Rate_Limiter('eth1', 128)
